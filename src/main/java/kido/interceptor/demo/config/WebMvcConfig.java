b717c9fb427bf5aefbdadafc87709d477995aa21
Việt Nguyễn <kido@Viets-MacBook-Pro.local>
2022-04-05 22:37:16 +0700

    Create controller and view test interceptor

--- src/main/java/kido/interceptor/demo/config/WebMvcConfig.java
+++ src/main/java/kido/interceptor/demo/config/WebMvcConfig.java
@@ -3,9 +3,11 @@ package kido.interceptor.demo.config;
 import kido.interceptor.demo.interceptor.AdminInterceptor;
 import kido.interceptor.demo.interceptor.LogInterceptor;
 import kido.interceptor.demo.interceptor.OldLogInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     //
     @Override
